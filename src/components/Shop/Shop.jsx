b8f62c9a2b6784d3c2cf1ebf180ec318bcb85f74
Mohammad Salim <mohammadsalim017427@gmail.com>
2024-05-08 03:48:06 -0700

    send pagination parameters

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -32,10 +32,10 @@ const Shop = () => {
      */
       console.log(count);
     useEffect(() => {
-        fetch('http://localhost:5000/products')
+        fetch(`http://localhost:5000/products?page=${currentPage}&size=${itemsPerPage}`)
             .then(res => res.json())
             .then(data => setProducts(data))
-    }, []);
+    }, [currentPage,itemsPerPage]);
 
     useEffect(() => {
         const storedCart = getShoppingCart();
